fdcb4cd7b967cec712615634a2c6a33342e707db
Aauustiin <longaustin2001@gmail.com>
2022-07-01 22:53:23 +0100

    improved the physics of the wall slide

--- Assets/Scripts/Player/GrabbingState.cs
+++ Assets/Scripts/Player/GrabbingState.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,13 +7,17 @@ using UnityEngine.InputSystem;
 public class GrabbingState : IPlayerState
 {
     private PlayerController player;
+    private float startTime;
 
     public GrabbingState(PlayerController player)
     {
         this.player = player;
     }
 
-    public void StateFixedUpdate() {}
+    public void StateFixedUpdate()
+    {
+        player.GetComponent<Rigidbody2D>().gravityScale = calculateGravity(2, Time.time - startTime);
+    }
 
     public IPlayerState HandleAction(InputAction.CallbackContext value)
     {
@@ -32,13 +37,19 @@ public class GrabbingState : IPlayerState
     {
         player.SwapState(new JumpingState(player, 0));
     }
+
+    private float calculateGravity(double aggresiveness, double x)
+    {
+        var dGravity = 0.5 + (0.5*Math.Tanh((aggresiveness*x)-2));
+        return (float)dGravity;
+    }
     
     public void OnEntry()
     {
         player.LetGo += OnLetGo;
+        startTime = Time.time;
         player.GetComponent<Animator>().Play("Base Layer.grab", 0, 0);
         player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        player.GetComponent<Rigidbody2D>().gravityScale = 0.5f;
     }
 
     public void OnExit()
